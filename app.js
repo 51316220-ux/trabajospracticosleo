6f88739d8bea678e8159110c164cac082b6bdde9
51316220-ux <51316220@ivt.edu.ar>
2026-06-09 11:19:59 -0300

    9 6

---
+++ app.js
@@ -0,0 +1,16 @@
+let numero1 = 330
+let numero2 = 30
+let suma=numero1+numero2
+console.log(numero1+'+'+numero2+'='+suma)
+alert(numero1+'+'+numero2+'='+suma)
+ numero1 = 45
+ numero2 = 15
+ let resta=numero1 - numero2
+alert(numero1+'-'+numero2+'='+ resta)
+ console.log(numero1+'-'+numero2+'='+ resta)
+let multiplicacion = numero1*numero2
+ console.log('el resultado de '+ numero1+'*'+numero2+' es'+ multiplicacion)
+ alert(numero1+'*'+numero2+'='+ multiplicacion)
+ let division=numero1/numero2
+ console.log('el resultado de '+ numero1+'/'+numero2+' es '+ division)
+ alert(numero1+'/'+numero2+'='+ division)
